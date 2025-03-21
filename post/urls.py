b16e79c2d0b8e785e4f4c41b703aeea69f2c936b
sanghun <vitash1215@gmail.com>
2025-03-21 14:12:16 +0900

    feat : 게시글 생성
    빌드 실패, 1차 완성

--- post/urls.py
+++ post/urls.py
@@ -1,7 +1,9 @@
 from backend.urls import path
 
 from . import views
+from .views import create_post
 
 urlpatterns = [
     path("", views.index),
+    path('posts/', create_post, name='create_post'),
 ]
